96cebeb1849392f7d594ee2090e0c2df1c9f131d
simesime3 <49155426+simesime3@users.noreply.github.com>
2025-04-12 07:46:53 +0900

    workflow

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import '../styles/globals.css';
+import "../styles/App.css";  // グローバルCSSのインポート
 import Header from "../components/Header/Header";  // ヘッダーコンポーネントのインポート
 
 function MyApp({ Component, pageProps }) {
