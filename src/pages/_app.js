b1706e3e299341cf7c0d634e478b6941bb2eaa64
simesime3 <49155426+simesime3@users.noreply.github.com>
2025-04-12 19:34:55 +0900

    deploy後の修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-// import '../styles/globals.css';
-// import "../styles/App.css";  // グローバルCSSのインポート
+import '../styles/globals.css';
+import "../styles/App.css";  // グローバルCSSのインポート
 import Header from "../components/Header/Header";  // ヘッダーコンポーネントのインポート
 
 function MyApp({ Component, pageProps }) {
